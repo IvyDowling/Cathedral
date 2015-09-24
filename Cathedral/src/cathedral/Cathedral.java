ac4fffabadbf7e1751b7151b1130ec93018a181f
MichaelIV <michaeldowlingiv@gmail.com>
2015-09-24 12:19:20 -0400

    first commit

---
+++ Cathedral/src/cathedral/Cathedral.java
@@ -0,0 +1,12 @@
+package cathedral;
+
+import rlengine.RLEngine;
+
+public class Cathedral {
+
+    public static void main(String[] args) {
+        RLEngine runtime = new RLEngine();
+        CombatSystem cs = new CombatSystem();
+    }
+
+}
