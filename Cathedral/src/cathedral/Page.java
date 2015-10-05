59e24bbeb2bca31e4a5cefd15f0490f6ad21d84d
MichaelIV <michaeldowlingiv@gmail.com>
2015-10-05 11:38:36 -0400

    made the page class hold an array of renders instead of render having an array for strings encap

--- Cathedral/src/cathedral/Page.java
+++ Cathedral/src/cathedral/Page.java
@@ -5,19 +5,26 @@ import java.awt.Color;
 
 public abstract class Page {
 
-    private Render defaultRender;
+    private Render[] defaultRender;
 
-    public Page(Render c) {
+    public Page(Render[] c) {
         if (!(c == null)) {
             defaultRender = c;
         }
     }
 
     public Page(String[] s) {
-        defaultRender = new Render(s, 0, 0, Color.BLACK, Color.WHITE);
+        defaultRender = new Render[s.length];
+        for (int i = 0; i < s.length; i++) {
+            defaultRender[i] = new Render(s[i], 0, 0, Color.BLACK, Color.WHITE);
+        }
     }
 
-    public Render getDefaultRender() {
+    public Render[] getDefaultRender() {
         return defaultRender;
+    }
+
+    public Render getDefatultRenderIndex(int i) {
+        return defaultRender[i];
     }
 }
